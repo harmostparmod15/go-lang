4d31adbf51e8ab76ae8b8f479f8630a45e090cf8
parmod <harmostparmod@gmail.com>
2025-08-17 08:39:46 +0530

    array and slices

---
+++ 4.array_slices/main.go
@@ -0,0 +1,37 @@
+package main
+
+func main() {
+
+	// fmt.Println("hello")
+
+	// -------  basic array declaration in go
+	// arr := []int{3, 42, 5, 6, 8}
+	// fmt.Printf(" cities %#V \n", arr)
+	// fmt.Println(arr[0])
+	// fmt.Printf("%v ", arr)
+
+	// ----------  separate 0s and 1s question on array
+	// numArr := []int{0, 1, 0, 0, 1, 0, 0, 1, 1, 1}
+	// i := 0
+	// j := len(numArr) - 1
+
+	// for i <= j {
+	// 	if numArr[i] == 1 && numArr[j] == 0 {
+	// 		k := numArr[i]
+	// 		numArr[i] = numArr[j]
+	// 		numArr[j] = k
+	// 	} else if numArr[i] == 0 {
+	// 		i++
+	// 	} else if numArr[j] == 1 {
+	// 		j--
+	// 	} else {
+	// 		i++
+	// 		j--
+	// 	}
+	// }
+
+	// for i := 0; i < len(numArr); i++ {
+	// 	fmt.Print(numArr[i], " ")
+	// }
+
+}
